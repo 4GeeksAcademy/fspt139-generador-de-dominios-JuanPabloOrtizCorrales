4e58c4c367a574b78a86775e66392b205974129c
kneloo <crisjedy1475@gmail.com>
2026-06-19 09:12:10 +0000

    generador de dominios completo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger', 'racoon'];
+const dot = ['.com','.net','.gov']
+
+const combinaciones = []
+
+for (let i = 0; i < pronoun.length; i++) {
+
+  for (let j = 0; j < adj.length; j++) {
+
+    for (let k = 0; k < noun.length; k++) {
+
+      for (let l = 0; l < dot.length; l++) {
+        
+
+        combinaciones.push(pronoun[i] + adj[j] + noun[k] + dot[l])
+      }
+      
+      
+    }
+    
+  }
+  
+}
+
+console.log(combinaciones);
+
+
+
+
+
+
 };
+
